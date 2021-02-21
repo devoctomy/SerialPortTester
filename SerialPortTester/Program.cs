0c4de36521b06869816ef40756ff980393cee9dd
Nick Pateman <nick@devoctomy.com>
2021-02-21 16:45:00 +0000

    Tweaks to make it easier to start a connection.

--- SerialPortTester/Program.cs
+++ SerialPortTester/Program.cs
@@ -11,27 +11,19 @@ namespace hello_serialport
         {
             await Task.Yield();
             var ports = new List<string>(SerialPort.GetPortNames());
-            var selectedPort = InputSelection("Please enter a serial port to use", ports);
-            var changePortSettings = InputSelection("Do you want co change the port settings?", new List<string>(new[] { "Yes", "No" }));
+            var selectedPort = InputSelection("Please enter a serial port to use", ports, null);
 
             var port = default(SerialPort);
-            if(changePortSettings == "Yes")
-            {
-                var baudRate = InputInteger("Please enter the baud rate to use");
-                var dataBits = InputInteger("Please enter the data bits to use");
-                var parity = InputSelection("Please enter the parity to use", new List<string>(Enum.GetNames(typeof(Parity))));
-                var stopBits = InputSelection("Please enter the parity to use", new List<string>(Enum.GetNames(typeof(StopBits))));
-                port = new SerialPort(
-                    selectedPort,
-                    baudRate,
-                    Enum.Parse<Parity>(parity, true),
-                    dataBits,
-                    Enum.Parse<StopBits>(stopBits, true));
-            }
-            else
-            {
-                port = new SerialPort(selectedPort);
-            }
+            var baudRate = InputInteger("Please enter the baud rate to use (Default = 9600)", 9600);
+            var dataBits = InputInteger("Please enter the data bits to use (Default = 8)", 8);
+            var parity = InputSelection("Please enter the parity to use (Default = None)", new List<string>(Enum.GetNames(typeof(Parity))), Parity.None.ToString());
+            var stopBits = InputSelection("Please enter the parity to use (Default = One)", new List<string>(Enum.GetNames(typeof(StopBits))), StopBits.One.ToString());
+            port = new SerialPort(
+                selectedPort,
+                baudRate,
+                Enum.Parse<Parity>(parity, true),
+                dataBits,
+                Enum.Parse<StopBits>(stopBits, true));
 
             port.Open();
 
@@ -60,7 +52,7 @@ namespace hello_serialport
                 {
                     break;
                 }
-                var txBuffer = System.Text.Encoding.ASCII.GetBytes($"{input}\n");
+                var txBuffer = System.Text.Encoding.ASCII.GetBytes($"{input}{GetEndPacket()}");
                 await port.BaseStream.WriteAsync(txBuffer, 0, txBuffer.Length);
                 Console.WriteLine($"TX :: {input}");
             }
@@ -71,9 +63,15 @@ namespace hello_serialport
             Console.ReadKey();
         }
 
+        private static string GetEndPacket()
+        {
+            return "\n";
+        }
+
         private static string InputSelection(
             string message,
-            List<string> options)
+            List<string> options,
+            string defaultValue)
         {
             Console.WriteLine(message);
             foreach (string curOption in options)
@@ -81,6 +79,11 @@ namespace hello_serialport
                 Console.WriteLine($"{options.IndexOf(curOption) + 1} - {curOption}");
             }
             var optionChoiceLine = Console.ReadLine();
+            if(!String.IsNullOrEmpty(defaultValue) && string.IsNullOrEmpty(optionChoiceLine))
+            {
+                return defaultValue;
+            }
+
             int optionChoice = -1;
             while (!int.TryParse(optionChoiceLine, out optionChoice) || (optionChoice < 0 || optionChoice > options.Count))
             {
@@ -89,21 +92,36 @@ namespace hello_serialport
                     Console.WriteLine($"{options.IndexOf(curOption) + 1} - {curOption}");
                 }
                 optionChoiceLine = Console.ReadLine();
+                if (!String.IsNullOrEmpty(defaultValue) && string.IsNullOrEmpty(optionChoiceLine))
+                {
+                    return defaultValue;
+                }
             }
 
             return options[optionChoice - 1];
         }
 
-        private static int InputInteger(string message)
+        private static int InputInteger(
+            string message,
+            int defaultValue)
         {
             Console.WriteLine(message);
             var choice = Console.ReadLine();
+            if(string.IsNullOrEmpty(choice))
+            {
+                return defaultValue;
+            }
+
             int value = -1;
             while (!int.TryParse(choice, out value))
             {
                 Console.Clear();
                 Console.WriteLine(message);
                 choice = Console.ReadLine();
+                if (string.IsNullOrEmpty(choice))
+                {
+                    return defaultValue;
+                }
             }
 
             return value;
